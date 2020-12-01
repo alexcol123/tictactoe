e019153cf5de0c9d9149be173d46835247c2d871
Henry Munoz <alexcol123@yahoo.com>
2020-12-01 13:13:11 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = ()=>{
+  return(
+    <>
+    <h1>Welcome to  react parcess micro</h1>
+    <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, dolorum?</h2>
+    </>
+  )
+}
+
+export default App;
